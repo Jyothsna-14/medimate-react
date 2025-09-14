39e6238ae2372a582cab640b04ad866b126a4a80
Jyothsna-14 <jyothsna.vs2005@gmail.com>
2025-09-14 12:00:40 +0530

    Add files via upload

---
+++ src/data/medicines.js
@@ -0,0 +1,30 @@
+const medicines = {
+  diabetes: [
+    { id: 1, name: "Blood Glucose Monitor", price: 799, image: "/images/glucose-monitor.png" },
+    { id: 2, name: "Test Strips", price: 2120, image: "/images/test-strips.png" },
+    { id: 3, name: "Ensure Diabetes Care", price: 1089, image: "/images/ensure.png" },
+    { id: 4, name: "Sugar Free Gold Plus", price: 339, image: "/images/sugarfree.png" }
+  ],
+  cardiac: [
+    { id: 1, name: "BP Monitor", price: 1599, image: "/images/bp-monitor.png" },
+    { id: 2, name: "Cardiac Support Tablets", price: 749, image: "/images/cardiac-tablets.png" }
+  ],
+  dental: [
+    { id: 1, name: "Toothpaste Sensodyne", price: 199, image: "/images/sensodyne.png" },
+    { id: 2, name: "Mouthwash", price: 149, image: "/images/mouthwash.png" }
+  ],
+  skinHair: [
+    { id: 1, name: "Hair Oil", price: 299, image: "/images/hair-oil.png" },
+    { id: 2, name: "Skin Cream", price: 399, image: "/images/skin-cream.png" }
+  ],
+  respiratory: [
+    { id: 1, name: "Inhaler", price: 599, image: "/images/inhaler.png" },
+    { id: 2, name: "Cough Syrup", price: 159, image: "/images/cough-syrup.png" }
+  ],
+  stomach: [
+    { id: 1, name: "Digene Tablets", price: 89, image: "/images/digene.png" },
+    { id: 2, name: "ORS Solution", price: 49, image: "/images/ors.png" }
+  ]
+};
+
+export default medicines;
