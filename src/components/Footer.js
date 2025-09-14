c2ea11c692107125d5cfcbd5b043d37c7575c3df
Jyothsna-14 <jyothsna.vs2005@gmail.com>
2025-09-14 11:59:08 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ background: "#1d3557", color: "white", padding: "10px", textAlign: "center" }}>
+      <p>© 2025 MedimateCare. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
